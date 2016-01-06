593ed774e29445b5da559aa73455c3e3be033128
KvasirSG <kennethheimann@gmail.com>
2016-01-06 09:01:10 +0100

    Update tree.cs

--- tree/tree.cs
+++ tree/tree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplication12
 {
@@ -11,13 +12,15 @@ namespace ConsoleApplication12
         static void Main(string[] args)
         {
             int træ = 1;
-            for (int plads = 0; plads < 10 ; plads++)
+            for (int plads = 0; plads < 25 ; plads++)
             {
-                Console.SetCursorPosition(20-plads, 5 + plads);
+                Console.SetCursorPosition((Console.WindowWidth/2)-plads, 5 + plads);
             
                 for (int i = 0; i < træ; i++)
                 {
-                    Console.Write("^");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("Å");
+                    Thread.Sleep(50);
                 }
                 træ = træ + 2;
             }
@@ -25,3 +28,4 @@ namespace ConsoleApplication12
         }
     }
 }
+
