9061658dc1ca57bfb754720f39b3f6d3459ba134
KvasirSG <kennethheimann@gmail.com>
2016-01-06 10:23:43 +0100

    Adding tree to a VS file

---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int træ = 1;
+            int tree = 25;
+
+            for (int plads = 0; plads < 25; plads++)
+            {
+                if (træ < 25)
+                {
+                    Console.SetCursorPosition(40 - plads, 5 + plads);
+
+
+                    for (int i = 0; i < træ; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("§");
+                        Thread.Sleep(50);
+                    }
+                    træ = træ + 2;
+                }
+                else
+                {
+                    Console.SetCursorPosition(16 + plads, 5 + plads);
+
+
+                    for (int i = 0; i < tree; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("§");
+                        Thread.Sleep(50);
+                    }
+                    tree = tree - 2;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
