3d7201f53b271edcc43d1c012eaf7ca3f7d3e1d2
KvasirSG <kennethheimann@gmail.com>
2016-01-06 09:35:04 +0100

    Update tree.cs

--- tree/tree.cs
+++ tree/tree.cs
@@ -12,20 +12,38 @@ namespace ConsoleApplication12
         static void Main(string[] args)
         {
             int træ = 1;
+            int tree = 25;
+
             for (int plads = 0; plads < 25 ; plads++)
             {
-                Console.SetCursorPosition((Console.WindowWidth/2)-plads, 5 + plads);
-            
-                for (int i = 0; i < træ; i++)
+                if (træ<25)
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("Å");
-                    Thread.Sleep(50);
+                    Console.SetCursorPosition(40- plads, 5 + plads);
+
+
+                    for (int i = 0; i < træ; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("§");
+                        Thread.Sleep(50);
+                    }
+                    træ = træ + 2;
                 }
-                træ = træ + 2;
+                else
+                {
+                    Console.SetCursorPosition(16+plads, 5 + plads);
+
+
+                    for (int i = 0; i < tree; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("§");
+                        Thread.Sleep(50);
+                    }
+                    tree = tree - 2;
+                }
             }
             Console.ReadKey();
         }
     }
 }
-
